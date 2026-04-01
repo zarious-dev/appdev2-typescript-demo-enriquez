eca00ecb9dadf7e914d1dc7c8520488cbdd5fa24
zarious-dev <enriquez.caezarie@gmail.com>
2026-04-01 15:25:01 +0800

    Done playing and updating the 06-alternative-to-enum.ts file

--- 06-alternative-to-enum.ts
+++ 06-alternative-to-enum.ts
@@ -2,10 +2,14 @@
     // Literal Types (Alternative to Enum)
     let userRole: 'admin' | 'guess' | 'teacher' | 'student' = 'admin'
 
-    userRole = 'guess'
-    // anotherUserRole = 'superadmin'
+    userRole = 'admin' // this is ok
+    // userRole = 'superadmin' // this is error. type "superadmin" is not assignable 
 
     function access(role: 'admin' | 'guess' | 'teacher' | 'student') {
-        // ...
+        console.log(`Access granted for ${role}`)
     }
-})
+    access('teacher')
+    access('student')
+
+    access(userRole)
+})()
